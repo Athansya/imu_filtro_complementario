5a740b78188bae5f9577d28aeb9e64185d3aac51
Athansya <atoriz98@outlook.com>
2024-02-29 12:40:48 -0600

    Guardado de cuaternion resultante

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@ Realsense D435i.
 */
 
 #include "complementary_filter.h"
+// #include <fstream>  // Descomentar lineas texto
 #include <iostream>
 #include <librealsense2/rs.hpp>
 #include <mutex>
+#include <vector>
 
 // Frecuencias de muestreo
 const int ACCEL_FPS = 250; // 63 o 250 Hz
@@ -22,13 +24,24 @@ try
 {
     std::cout << "Estimación de pose" << std::endl;
 
+    // Archivo de texto
+    // std::ofstream outfile;
+    // outfile.open("../valores_cuaternion.txt");
+    // bool writeHeader = true;
+
     // Filtro complementario
     imu_tools::ComplementaryFilter CF;
     rs2_vector gyro_data, accel_data;
     bool firstIteration = true;
     double dt_init = 0.0;
-    // Cuaternión de rotación
+    // Cuaternión resultante
     double q0, q1, q2, q3;
+    // double q0_inv, q1_inv, q2_inv, q3_inv;
+
+    // Cuaterniones 
+    std::vector<double> quaternionRotationCF(4, 0.0);
+    std::vector<double> quaternionRotationCFConjugate(4, 0.0);
+    std::vector<double> quaternionPureAccel(4, 0.0);
 
     // Realsense IMU config
     rs2::config cfg;
@@ -47,7 +60,7 @@ try
         // Convertir frame a tipo IMU
         auto motion = frame.as<rs2::motion_frame>();
 
-        // Gyroscopio
+        // Giroscopio
         if (motion && motion.get_profile().stream_type() == RS2_STREAM_GYRO &&
             motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
         {
@@ -70,13 +83,43 @@ try
     {
         // Actualización del filtro complementario
         CF.update(accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z, GYRO_DT);
+
         // Cuaternión resultante
         CF.getOrientation(q0, q1, q2, q3);
+        // quaternionRotationCF[0] = q0;
+        // quaternionRotationCF[1] = q1;
+        // quaternionRotationCF[2] = q2;
+        // quaternionRotationCF[3] = q3;
 
         std::cout << "Cuaternión: (" << q0 << ", " << q1 << ", " << q2 << ", " << q3 << ")" << std::endl;
+
+        // imu_tools::invertQuaternion(q0, q1, q2, q3, q0_inv, q1_inv, q2_inv, q3_inv);
+
+
+        // Guardamos valores cuaternion
+        // TODO: Revisar por qué está guardando más de
+        // 400,000 valores en tan poco tiempo!
+        // if (outfile.is_open())
+        // {
+            // if (writeHeader)
+            // {
+                // outfile << "q0,q1,q2,q3\n";
+                // writeHeader = false;
+                // continue;
+            // }
+
+            // outfile << q0 << "," << q1 << "," << q2 << "," << q3 << "\n";
+        // }
+        
+        // Calculamos posición
+
     }
 
+    // Cerramos archivo
+    // outfile.close();
+
     // Paramos el pipeline RS2
+
     pipe.stop();
 
     return EXIT_SUCCESS;
